de44e4996fc2e93e2e44e71245bcbd60701fc4b8
ElliottCepin <elliottcepin5@gmail.com>
2026-05-12 10:09:34 -0700

    set up interface and subtest

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -1,6 +1,21 @@
 package store
 import (
 	"github.com/ElliottCepin/go-sqlite-pastebin/internal/server"
+	"testing"
 )
 
+func subtestRoundTrip(t *testing.T, s server.Store) {
+	in := "abcd"
+	out := "efgh"
+	if err := s.Save(t.Context(), in, out); err != nil {
+		t.Errorf("An unexpected error occured during s.Save: %v", err)
+	}
+	val, err := s.Get(t.Context(), in)
+	if (err != nil) {
+		t.Errorf("An unexpected error occured during s.Get: %v", err)
+	}
 
+	if (val != out) {
+		t.Errorf("s.Get returned the wrong value. Expected %v, got %v", out, val)
+	}
+}
